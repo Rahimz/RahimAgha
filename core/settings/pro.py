53660924293c0d19f0594e68bd14c2a92168c261
rahim <rahim.aghareb@gmail.com>
2022-12-26 13:31:24 +0330

    production settings added

---
+++ core/settings/pro.py
@@ -0,0 +1,11 @@
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'rahimagha.ir', 
+    # 'www.computermuseum.ir', 'localhost', server_ip
+    ]
+
+# CSRF_TRUSTED_ORIGINS = ['https://*.ketabedamavand.com']
