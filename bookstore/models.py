2b94dcca9a0ee40b235106f6d65a991a3928b380
rahim <rahim.aghareb@gmail.com>
2022-12-29 15:29:09 +0330

    imporve - Feature model added

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        auto_now_add=True
+    )
+    modified = models.DateTimeField(
+        auto_now=True
+    )
+    class Meta:
+        abstract = True
+
+
+class Feature(TimeStampedModel):
+    title = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    tags = models.CharField(
+            max_length=200,
+            null=True,
+            blank=True
+    )
+    show_rank = models.PositiveIntegerField(
+        default=10
+    )
+
+    class Meta:
+        ordering = ('show_rank', 'id')
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    feature = models.ForeignKey(
+        'Feature',
+        on_delete=models.CASCADE,
+        related_name='feature_images',
+    )
+    title = models.CharField(
+        max_length=150,
+    )
+    image = models.ImageField(
+        upload_to='images/'
+    )
+    image_alt = models.CharField(
+        max_length=150,
+        null=True, 
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.title
+
+
+class Point(models.Model):
+    feature = models.ForeignKey(
+        'Feature',
+        on_delete=models.CASCADE,
+        related_name='feature_points',
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content[:25]
--- core/settings/base.py
+++ core/settings/base.py
@@ -128,3 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media/')
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
