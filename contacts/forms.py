22aec358a287632dd53df4af80f749c5748ce5f5
rahim <rahim.aghareb@gmail.com>
2023-01-05 18:45:21 +0330

    translation to farsi added

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Contact
 
@@ -7,3 +8,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ("title","description", "phone", "email")
+        labels = {
+            "title": _("Tilte"),
+            "description": _("Description"),
+            "email": _("Email"),
+            "phone": _("Phone"),
+        }
