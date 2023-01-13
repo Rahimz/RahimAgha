f287ba8a07424795f8579783204b5b79e6103f77
rahim <rahim.aghareb@gmail.com>
2023-01-13 13:20:41 +0330

    improve - quize maker is created

--- quizes/urls.py
+++ quizes/urls.py
@@ -6,5 +6,10 @@ app_name = 'quizes'
 
 urlpatterns = [
     path('questions/create/', login_required(views.QuestionCreateView.as_view()), name='question_create' ),
-    path('questions/list/', views.QuestionListView.as_view(), name='questions_list' ),
+    path('questions/list/', login_required(views.QuestionListView.as_view()), name='questions_list' ),
+
+    path('my-quiz/', views.MyQuizView, name='my_quiz' ), #also uses for start a quiz
+    
+    path('my-quiz/<uuid:pk>/', views.MyQuizView, name='My_quiz_proccess' ),
+    path('my-quiz/result/<uuid:pk>/', views.QuizResultView, name='My_quiz_result' ),
 ]
--- quizes/views.py
+++ quizes/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.mixins import LoginRequiredMixin
+import random 
 
 from django.forms import Select
 
@@ -35,3 +36,56 @@ class QuestionListView(ListView):
         context['page_title'] = _('Questions list')
         context['mainNavSection'] = 'quizes' 
         return context
+
+def MyQuizView(request):
+    # print(request.GET.get('start'), type(request.GET.get('start')))
+    if request.GET.get('start') == 'True' and request.session.get('quiz_completed', True):
+        questions = SelectQuestion('3')
+        # quiz = Quiz.objects.crreate(
+        #     difficulty='1',
+        #     questions=questions
+        # )
+        print(questions)
+
+    
+        
+    return render(
+        request,
+        'quizes/my_quiz.html',
+        {
+            'page_title': _('My quiz'),
+        }
+    )
+
+
+def SelectQuestion(level):
+    """
+    This function makes a list of 5 questions id
+    and returns it in a list
+    """
+    qs_easy = list(Question.objects.filter(difficulty__in=['1']).values_list('pk', flat=True))
+    qs_medium = list(Question.objects.filter(difficulty__in=['2', '3']).values_list('pk', flat=True))
+    qs_hard = list(Question.objects.filter(difficulty__in=['4', '5']).values_list('pk', flat=True))
+
+    if level == '1':
+        questions = random.sample(qs_easy, 1) + random.sample(qs_medium, 3) + random.sample(qs_hard, 1)
+    if level == '2':
+        questions = random.sample(qs_easy, 1) + random.sample(qs_medium, 2) + random.sample(qs_hard, 2)
+    if level == '3':
+        questions = random.sample(qs_easy, 1) + random.sample(qs_medium, 1) + random.sample(qs_hard, 3)
+    return questions
+
+
+def QuizResultView(request, pk):
+    quiz = get_object_or_404(Quiz, pk=pk)
+    if not quiz.completed:
+        quiz = None
+    
+    return render(
+        request,
+        'quiz/question_list.html',
+        {
+            'quiz': quiz,
+            'page_title': _('Quiz results')
+        }
+    )
