3b99181d9aa5b0d774cee18c391ad1921cbaa368
rahim <rahim.aghareb@gmail.com>
2023-05-26 12:16:23 +0330

    sentry added

--- core/settings/base.py
+++ core/settings/base.py
@@ -181,3 +181,24 @@ EMAIL_SUBJECT_PREFIX = '[RahimAgha] '
 
 ### Accounting
 DEFAULT_MAX_DIGIT = 20
+
+
+# settings for sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
