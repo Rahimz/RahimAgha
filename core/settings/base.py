16e67bb239c7709666a9f0040ff5966385281533
rahim <rahim.aghareb@gmail.com>
2024-10-15 11:01:52 +0330

    update api code

--- core/settings/base.py
+++ core/settings/base.py
@@ -225,4 +225,3 @@ CSRF_TRUSTED_ORIGINS = [
      "http://127.0.0.1:8000",
 ]
 
-IPLocation_KEY = IPLocation_API_KEY
--- generals/views.py
+++ generals/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
-from django.conf import settings
 
 import requests
 from django.http import JsonResponse
 from django.views import View
 
 from .models import GeoRecord
+from core.settings.secret import API_KEY_LOCATION
 
 def HomeView(request):
     # if request.GET.get('SwitchNight') == 'on':
@@ -22,31 +22,33 @@ def HomeView(request):
     )
 
 
-
-
 class GetCountryFromIP(View):
     def get(self, request, ip):
         # Validate the IP address format (optional, but recommended)
         if ip == '127.0.0.1':
+            # print('local')
             return JsonResponse({'country': 'IR'})
         if not self.is_valid_ip(ip):
             return JsonResponse({'error': 'Invalid IP address'}, status=400)
         
         if GeoRecord.objects.filter(ip=ip).exists():
+            # print('in databse')
             record = GeoRecord.objects.filter(ip=ip).latest('created')
             record.count += 1 
             record.save()
             return JsonResponse({'country': record.country}, status=200)
             
-        # Make a request to the ipinfo.io API
+        # Make a request 
         try:
+            # print('in api')
             # response = requests.get(f"https://ipinfo.io/{ip}/json")
-
-            response = requests.get(f"https://api.ip2location.io/?key={settings.IPLocation_KEY}&ip={ip}")
+            # country = response.json().get('country')
+            
+            response = requests.get(f"https://api.ip2location.io/?key={API_KEY_LOCATION}&ip={ip}")
             response.raise_for_status()  # Raise an error for bad responses
 
             # Extract country information
-            country = response.json().get('country')
+            country = response.json().get('country_code')
 
             try:
                 record = GeoRecord.objects.create(
@@ -61,6 +63,7 @@ class GetCountryFromIP(View):
             return JsonResponse({'country': country}, status=200)
 
         except requests.RequestException as e:
+            # print('in api exception')
             return JsonResponse({'error': str(e)}, status=500)
 
     def is_valid_ip(self, ip):
