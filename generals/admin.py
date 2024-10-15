9248ff6850a62b9a47c5cbc918bb6148c8fd2c3d
rahim <rahim.aghareb@gmail.com>
2024-10-15 11:19:26 +0330

    refactor updated in geolocation model

--- generals/admin.py
+++ generals/admin.py
@@ -5,7 +5,7 @@ from .models import GeoRecord
 
 @admin.register(GeoRecord)
 class GeoRecordAdmin(admin.ModelAdmin):
-    list_display = ['id', 'country', 'ip', 'count', 'created']
+    list_display = ['id', 'country', 'ip', 'count', 'updated',  'created']
     list_filter = ['country']
     search_fields = ['country', 'ip']
     date_hierarchy = 'created'
--- generals/models.py
+++ generals/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class GeoRecord(models.Model):
     ip = models.GenericIPAddressField()
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)    
+    updated = models.DateTimeField(auto_now=True)    
     country= models.CharField(
         max_length=10
     )
