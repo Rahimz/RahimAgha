0f303b43725aa68057665c706b7f112e19abb965
rahim <rahim.aghareb@gmail.com>
2024-11-14 22:25:43 +0330

    add mark as spam to Contact admin list

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib import messages 
 
 from .models import Contact
+
+
+def mark_as_spam(modeladmin, request, queryset):
+    # Perform your action here
+    # For example, let's say you want to mark selected objects as active
+    queryset_count = queryset.count()
+    queryset.update(is_spam=True)  # Assuming your model has an 'active' field
+    messages.success(request, f"{queryset_count} items marked as spam.")
+
+mark_as_spam.short_description = "Mark selected items as spam"  # This will be the name displayed in the admin
 
 
 
@@ -13,4 +24,5 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = ['title', 'phone', 'email', 'description']
     # inlines = [ImageInline, PointInline]
     list_filter = ['is_checked', 'is_spam']
+    actions = [mark_as_spam]
 
