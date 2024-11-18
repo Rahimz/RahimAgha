c398032cc26c40029075fe74b65e25afeb8daca0
rahim <rahim.aghareb@gmail.com>
2024-11-18 08:18:32 +0330

    add logging

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -1,5 +1,5 @@
 from .base import *
-
+import logging.config
 
 DEBUG = False
 
@@ -22,3 +22,41 @@ CORS_ALLOWED_ORIGINS = [
 
 
 SERVER_EMAIL = 'noreply@rahimagha.ir'
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'rotating_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_errors.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 10,  # Keep 10 backup files
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['rotating_file', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
