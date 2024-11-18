aab43c2a3710ecbdde79c7d9f883f6ec3c5eed61
rahim <rahim.aghareb@gmail.com>
2024-11-18 08:52:57 +0330

    refactor video list

--- videos/views.py
+++ videos/views.py
@@ -35,7 +35,7 @@ def VideoListView(request, category_id=None):
     context = dict(
         page_title='All videos'
     )
-    videos = Video.objects.all().order_by('-1d')
+    videos = Video.objects.all().order_by('-created')
     if category_id:
         try:
             category = Category.objects.get(id=category_id)
