abafd044290c473671fa9603b680bf1e4f28e292
rahim <rahim.aghareb@gmail.com>
2024-11-18 09:28:14 +0330

    update video admin

--- videos/admin.py
+++ videos/admin.py
@@ -6,6 +6,7 @@ from .models import Video
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
-    list_display = ['name', 'id', 'is_protected', 'category', 'website_header']
+    list_display = ['name', 'id', 'is_protected', 'category', 'website_header', 'created']
     search_fields = ['name', 'id']
     list_editable = ['is_protected', 'website_header']
+    ordering = ('-created',)
