cebfc969f8aa0d1938fd07ca8edc4c390d51694d
rahim <rahim.aghareb@gmail.com>
2025-07-27 18:57:02 +0330

    ai form updated

--- ai/urls.py
+++ ai/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'ai'
 
 urlpatterns = [
-    path('', views.AiView, name='ai')
+    path('create/', views.AiCreateNewChatView, name='ai_create'),
+    path('continue/<str:chat_id>/', views.AiCreateNewChatView, name='ai_continue_chat'),
+    path('', views.AiView, name='ai'),
 ]
--- ai/views.py
+++ ai/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.conf import settings
+from django.contrib import messages
 
 from langchain_openai import ChatOpenAI
 
 from .forms import ChatForm
+from .models import Chat, Message
 
 API_KEY = settings.AVAL_API_KEY
 
@@ -13,7 +15,7 @@ def AiView(request):
     context = dict(
         page_title = 'ai'
     )
-    messages = None
+    ai_messages = None
     ai_message = None
     # Initialize the ChatOpenAI model
     model_name = "gpt-4o-mini"
@@ -25,22 +27,22 @@ def AiView(request):
         form = ChatForm(data=request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            # Initialize messages list with system and user prompts
-            messages = [        
+            # Initialize ai_messages list with system and user prompts
+            ai_messages = [        
                 {"role": "user", "content": cd['prompt']},
             ]
-            # Initialize messages list with system and user prompts
-            # messages = [        
+            # Initialize ai_messages list with system and user prompts
+            # ai_messages = [        
             #     {"role": "user", "content": "in less than 30 words define django rest framework serializer"},
             # ]
             
-            # ai_message = llm.invoke(messages)
+            # ai_message = llm.invoke(ai_messages)
             form = ChatForm()
     else: 
         form = ChatForm()
     context.update(
         model_name=model_name,
-        messages=messages,
+        ai_messages=ai_messages,
         ai_message=ai_message,
         form=form
     )
@@ -51,7 +53,94 @@ def AiView(request):
     )
 
 
-
+def AiCreateNewChatView(request, chat_id=None):
+    context = dict(
+        page_title = 'create ai chat',
+        chats=Chat.objects.all().order_by('-id'),
+    )
+    ai_messages = None
+    ai_message = None
+    chat=None
+    if chat_id:
+        try:
+            chat=get_object_or_404(Chat, chat_id=chat_id)
+            ai_messages = Message.objects.filter(chat=chat)
+        except:
+            pass
+    
+    
+    # Initialize the ChatOpenAI model
+    model_name = "gpt-4o-mini"
+    # model_name = "claude-3-opus"
+    
+    llm = ChatOpenAI(
+        model=model_name, base_url="https://api.avalai.ir/v1", api_key=API_KEY
+    )
+    
+    if request.method == 'POST':
+        form = ChatForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # Initialize ai_messages list with system and user prompts
+            ai_messages = [        
+                {"role": "user", "content": cd['prompt']},
+            ]
+            chat = Chat.objects.create(
+                chat_id='working',
+                model_name=model_name,
+                input_token=0,
+                output_token=0,
+                total_token=0,
+            )
+            print('.. create chat body')
+            message = Message.objects.create(
+                chat=chat,
+                role='user',
+                content=cd['prompt']
+            )
+            print('.. create first chat')
+            try:
+                ai_message = llm.invoke(ai_messages)
+                pass
+            except Exception as e:
+                print(str(e))
+            
+            # ai_message = llm.invoke(ai_messages)
+            
+            if ai_message:
+                print('.. get ai response')
+                chat.chat_id = ai_message.id
+                chat.input_token=ai_message.usage_metadata["input_tokens"]
+                chat.output_token=ai_message.usage_metadata["output_tokens"]
+                chat.total_token=ai_message.usage_metadata["total_tokens"]
+                chat.save()
+                
+                prompt_content = ai_message.content
+                answer_message = Message.objects.create(
+                    chat=chat,
+                    role='assistant',
+                    content=prompt_content,
+                )
+                messages.success(request, 'new chat created')
+                
+                return redirect('ai:ai_continue_chat', chat.chat_id)
+            
+    else: 
+        form = ChatForm()
+    context.update(
+        model_name=model_name,
+        ai_messages=ai_messages,
+        ai_message=ai_message,
+        form=form,
+        chat=chat,
+    )
+    return render(
+        request,
+        'ai/ai_chat.html',
+        context
+    )
+    
+    
 
 
 
