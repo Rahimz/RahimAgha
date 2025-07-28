e76cacead07abd353e2a22f7d0310416908bf240
rahim <rahim.aghareb@gmail.com>
2025-07-28 09:04:15 +0330

    add code hilite for response

--- ai/templatetags/markdown_extras.py
+++ ai/templatetags/markdown_extras.py
@@ -5,4 +5,17 @@ register = template.Library()
 
 @register.filter(name='markdown')
 def markdown_to_html(text):
-    return markdown.markdown(text)
+    extensions = [
+        'fenced_code',
+        'codehilite',  # Requires Pygments CSS
+        'extra',
+    ]
+    extension_configs = {
+        'codehilite': {
+            'use_pygments': True,
+            'css_class': 'codehilite',
+            'guess_lang': True,      # Try to guess language if not specified
+            'linenums': False,      # Disable line numbers
+        }
+    }
+    return markdown.markdown(text, extensions=extensions, extension_configs=extension_configs)
--- ai/urls.py
+++ ai/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'ai'
 
 urlpatterns = [
-    path('create/', views.AiCreateNewChatView, name='ai_create'),
-    path('continue/<str:chat_id>/', views.AiCreateNewChatView, name='ai_continue_chat'),
-    path('', views.AiView, name='ai'),
+    path('test/', views.AiView, name='ai'),
+    path('<str:chat_id>/', views.AiCreateNewChatView, name='ai_continue_chat'),
+    path('', views.AiCreateNewChatView, name='ai_create'),
 ]
