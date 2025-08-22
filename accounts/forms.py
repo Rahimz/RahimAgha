d848192c23d2c801a25453962a4c11dc39f54c7f
rahim <rahim.aghareb@gmail.com>
2025-08-22 10:53:13 +0330

    add registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,25 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
-# class CustomUserCreationForm(UserCreationForm):
-#     class Meta:
-#         model = User
-#         fields = ("username",) # add others if you want
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput
+    )
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'email']
+    
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
+    
+    
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
+from . import views
 # from django.contrib.auth import views as auth_views
 # from .views import RegisterView
-# from . import views
 
 
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
-    # path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
 ]
