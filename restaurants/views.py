46399c5cae5072d0eb931b09f5f4c21284dc76aa
rahim <rahim.aghareb@gmail.com>
2025-10-01 11:14:21 +0330

    add map and tags

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.translation import gettext_lazy as _
+from taggit.models import Tag
+
+from restaurants.models import Place
+
 
 def ResHomeView(request):
+    city = request.GET.get('city', None)
+    tag = request.GET.get('tag', None)
+    places = Place.actives.all()
+    if city:
+        places = places.filter(city=city)
+        print('... gett city', city)
+        
+    if tag:
+        print('... gett tag', tag)
+        try:
+            tag_obj = Tag.objects.get(slug=tag)
+            print('... gett tag object', tag_obj)
+        except:
+            tag_obj = None
+        if tag_obj:
+            places= places.filter(tags__in=[tag_obj])        
     context = dict(
         page_title = _("Restaurants"),
+        places=places,
+        cities=Place.actives.order_by('city').distinct('city').values_list('city', flat=True),
+        city=city,
+        tag=tag,
     )
     return render(
         request,
