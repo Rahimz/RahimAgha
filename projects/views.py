4eeafc04d9f30e5f8fe6ebc9d01febf1d08c1d12
rahim <rahim.aghareb@gmail.com>
2025-12-10 12:07:23 +0330

    update projects page

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
+from django.utils.translation import gettext_lazy as _
 
+from projects.models import ProjectCategory
 
-class ProjectView(TemplateView):
+class ProjectView(ListView):
+    
+    model = ProjectCategory
+    queryset = ProjectCategory.objects.all()
     template_name = 'projects/projects.html'
+    context_object_name = 'projects'
+    
+    def get_queryset(self):
+        return super().get_queryset().prefetch_related('projects')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = _("Project")
+        
+        return context
+    
     
